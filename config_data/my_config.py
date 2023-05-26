d4645f99b23f71c2f45a68e66e7f92e3c0ef0a29
talgat_moldazhanov <talgat_0508@gmail.com>
2023-05-26 07:19:42 +0600

    Initial commit

--- config_data/my_config.py
+++ config_data/my_config.py
@@ -10,8 +10,8 @@ else:
     logger.info(f'{logger.name}')
     load_dotenv()
 
-BOT_TOKEN = os.getenv('5852786867')
-RAPID_API_KEY = os.getenv("4039428")
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+RAPID_API_KEY = os.getenv("RAPID_API_KEY")
 DEFAULT_COMMANDS = (
     ('start', "Запустить бота"),
     ('help', "Вывести справку"),
